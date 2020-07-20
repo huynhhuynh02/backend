2d099701ed520ecb7c39710b2cdd6fba29c62f54
lephuoccanh <lephuoccanh@gmail.com>
2020-07-20 20:02:36 +0700

    Add telegram notification.

--- test/env.js
+++ test/env.js
@@ -1,6 +1,10 @@
 process.env.DB_HOST = "127.0.0.1";
 process.env.DB_USERNAME = "root";
-process.env.DB_PASSWORD = "";
+if (process.env.TRAVIS_ENV === '1') {
+  process.env.DB_PASSWORD = "";
+} else {
+  process.env.DB_PASSWORD = "12345678";
+}
 process.env.DB_NAME = "yocto_erp_test";
 process.env.WEB_URL = "http://localhost:4201";
 process.env.NODE_ENV = "test";
