5e6942f6c0d456a8ba155ebf1ae021af892ff923
lephuoccanh <lephuoccanh@gmail.com>
2020-07-20 15:24:10 +0700

    Add test/env.js

---
+++ test/env.js
@@ -0,0 +1,6 @@
+process.env.DB_HOST = "127.0.0.1";
+process.env.DB_USERNAME = "root";
+process.env.DB_PASSWORD = "12345678";
+process.env.DB_NAME = "yocto_erp_test";
+process.env.WEB_URL = "http://localhost:4201";
+process.env.NODE_ENV = "test";
