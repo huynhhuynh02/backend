7e2e39dcd3f8986e69393a2690ffd5eef88f513b
lephuoccanh <lephuoccanh@gmail.com>
2020-07-27 10:00:51 +0700

    [skip travis] - Asset Service.

--- app/service/asset/asset.service.js
+++ app/service/asset/asset.service.js
@@ -1,69 +1,53 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import db from '../../db/models';
 import ProductAsset from '../../db/models/product/product-asset';
 
 const fs = require("fs");
 
-export async function convertBase64(baseImage) {
-  // console.log(baseImage);
-  console.log('vsdfsdfsdfsdfdsfdsfds');
-  const uploadPath = "./uploads";
-  const localPath = `${uploadPath}/`;
-  const ext = baseImage.substring(baseImage.indexOf("/")+1, baseImage.indexOf(";base64"));
-  const fileType = baseImage.substring("data:".length,baseImage.indexOf("/"));
-  const regex = new RegExp(`^data:${fileType}\/${ext};base64,`, 'gi');
+const ASSET_STORE_FOLDER = './uploads';
+
+if (!fs.existsSync(`${ASSET_STORE_FOLDER}/`)) {
+  fs.mkdirSync(`${ASSET_STORE_FOLDER}/`);
+}
+
+export async function storeFileFromBase64(baseImage) {
+  const ext = baseImage.substring(baseImage.indexOf("/") + 1, baseImage.indexOf(";base64"));
+  const fileType = baseImage.substring("data:".length, baseImage.indexOf("/"));
+  const regex = new RegExp(`^data:${fileType}/${ext};base64,`, 'gi');
   const base64Data = baseImage.replace(regex, "");
-  // const filename = `${uuidv4()}.${ext}`;
   const filename = uuidv4();
-  if(!fs.existsSync(`${uploadPath}/`)) {
-    fs.mkdirSync(`${uploadPath}/`);
-  }
-  if (!fs.existsSync(localPath)) {
-    fs.mkdirSync(localPath);
-  }
-  fs.writeFileSync(localPath+filename, base64Data, 'base64');
+
+  fs.writeFileSync(`${ASSET_STORE_FOLDER}/${filename}`, base64Data, 'base64');
   return filename;
 }
 
-export async function createAsset(productId, createForm, transaction) {
+export async function createProductAsset(productId, assetsForm, transaction) {
   const opts = {};
   if (transaction) {
     opts.transaction = transaction;
   }
-  for (let i = 0; i < createForm.length; i += 1) {
+
+  const assets = [];
+  for (let i = 0; i < assetsForm.length; i += 1) {
     // eslint-disable-next-line no-await-in-loop
-    const fileId = await convertBase64(createForm[i].data);
-    // eslint-disable-next-line no-await-in-loop
-    const assets = await db.Asset.create({
-      name: createForm[i].name,
-      type: createForm[i].type,
-      ext: createForm[i].ext,
-      size: createForm[i].size,
+    const fileId = await storeFileFromBase64(assetsForm[i].data);
+
+    assets.push({
+      name: assetsForm[i].name,
+      type: assetsForm[i].type,
+      ext: assetsForm[i].ext,
+      size: assetsForm[i].size,
       fileId: fileId,
       createdDate: new Date()
-    }, opts);
-    // eslint-disable-next-line no-await-in-loop
-    await ProductAsset.create({
-      assetId: assets.id,
-      productId: productId
-    }, opts);
+    });
   }
-   // const assets = await db.Asset.bulkCreate(createForm.map(result => {
-   //   return  {
-   //     name: result.name,
-   //     type: result.type,
-   //     ext: result.ext,
-   //     size: result.size,
-   //     fileId: uuidv4(),
-   //     createdDate: new Date()
-   //   }
-   // }), opts);
-   //
-   //  await db.ProductAsset.bulkCreate(assets.map(result => {
-   //    return {
-   //      assetId: result.id,
-   //      productId: productId
-   //    }
-   //  }), opts);
+  const assetModels = await db.Asset.bulkCreate(assets, {transaction});
 
+  return ProductAsset.bulkCreate(
+    assetModels.map(t => {
+      return {
+        assetsId: t.id,
+        productId: productId
+      }
+    }), {transaction});
 }
